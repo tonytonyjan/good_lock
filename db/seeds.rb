0f8293295a866bcdd0d85fcc43959649edaac234
Tony Jian <tonytonyjan@gmail.com>
2013-11-02 18:51:00 +0800

    fix seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,9 +3,9 @@ news_seed_path = Rails.root.join('db', 'news_seed.json')
 JSON.parse(IO.read(news_seed_path)).each do |news|
   news_item = case news['tj_type']
   when 'FlickrItem'
-    FlickrItem.new_from_flickr_item news
+    FlickrItem.new_from_flickr_item news.except!('tj_type')
   else 'YahooItem'
-    YahooItem.new_from_feed_item news
+    YahooItem.new_from_feed_item news.except!('tj_type')
   end
   news_item.save
 end if File.exist? news_seed_path
