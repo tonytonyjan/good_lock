c2afe6beaa2518587d3832bd1d79308509525a7d
Tony Jian <tonytonyjan@gmail.com>
2013-11-02 13:41:45 +0800

    fix column

--- db/migrate/20131102050648_create_news_items.rb
+++ db/migrate/20131102050648_create_news_items.rb
@@ -1,7 +1,7 @@
 class CreateNewsItems < ActiveRecord::Migration
   def change
     create_table :news_items do |t|
-      t.string :guid
+      t.text :guid
       t.string :title
       t.text :description
       t.string :link
