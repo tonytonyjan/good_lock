a019a8501dc1b59588eca02a6f046a9de2ba0660
Tony Jian <tonytonyjan@gmail.com>
2013-11-02 14:08:14 +0800

    annotate

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: events
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer          not null
+#  event_id   :string(255)      not null
+#  state      :string(255)      default("unset"), not null
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Event < ActiveRecord::Base
   belongs_to :user
   extend Enumerize
