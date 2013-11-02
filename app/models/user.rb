19941b7ae216fa137ca79eecb341d71abbc9cf68
Tony Jian <tonytonyjan@gmail.com>
2013-11-02 10:48:35 +0800

    annotate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,19 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id            :integer          not null, primary key
+#  uid           :string(255)
+#  name          :string(255)
+#  email         :string(255)
+#  image         :string(255)
+#  token         :text
+#  refresh_token :text
+#  expires_at    :integer
+#  expires       :boolean
+#  created_at    :datetime
+#  updated_at    :datetime
+#
+
 class User < ActiveRecord::Base
 end
