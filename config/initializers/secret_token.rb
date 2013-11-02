c4b787fe91325ad0675bba65827ce02ba7f74821
Tony Jian <tonytonyjan@gmail.com>
2013-11-02 10:24:34 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+YahooHack::Application.config.secret_key_base = '2d2f9f5307655f20a33f35878134fd6002a0bb7c64ad15b50bde9ad81e6b4cb7b37f04b08fc823b420abcda915d71f5dbe9a3c9357789622cfe6ef3741ad0eae'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+YahooHack::Application.config.session_store :cookie_store, key: '_yahoo_hack_session'
